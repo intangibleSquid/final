40f5c7e7f7743f37df3a6c1fad16ca48952d2a37
intangibleSquid <ccoy2@ivytech.edu>
2022-07-24 16:09:23 -0400

    main/myGUI
    Main program function/ root GUI window defined and version 1 of myGUI.py is finished

--- main.py
+++ main.py
@@ -5,16 +5,41 @@ Description:
 """
 # import statements
 from myGUI import *
+from PIL import ImageTk, Image
 
-# define main function
+
+# main function
 def main():
-    """main program function description here"""
-    # root gui window
-    root = MyWindow('test', '600x400')
-    
-    main_frame = MyLabelFrame(root, 'test', row=0, col=0)
-    a = MyButton(main_frame, 'test', row=0, col=0)
-    # help(MyWindow)
+    """main function / root window of the pizza ordering program"""
+    # create program's root GUI window
+    root = MyWindow("Peter's Pizza Palace - Priority order-Placement Program", '600x420')
+
+    # create program's logo and welcome text
+    logo = ImageTk.PhotoImage(Image.open('images/logo.png'))  # open logo image
+    logo_label = MyLabel(root, 'logo', row=0, col=0, img=logo)  # place logo image
+    welcome = MyLabel(root, "Welcome to Peter's Pizza Palace! Let's get started taking your order today!\n",
+                      row=1, col=0)  # welcome text
+
+    # order pick-up options
+    pickup = MyLabel(root, "How would you like to receive your order today?", row=2, col=0)  # ask about pick-up
+    pickup_frame = MyFrame(root, row=3, col=0)  # frame to hold pick-up options
+    delivery = MyRadio(pickup_frame, 'Delivery', row=0, col=1)  # delivery option
+    carry = MyRadio(pickup_frame, 'Carry-Out', row=0, col=2)  # carry-out option
+
+    # program navigation
+    nav_frame = MyFrame(root, row=4, col=0)  # frame to hold nav buttons
+
+    # 'start order' button's function
+    def start_order():
+        return
+    start_order = MyButton(nav_frame, "Start Order", row=0, col=0, command=start_order)  # 'start order' button
+
+    # 'exit program' button's function
+    def exit_prog():
+        """closes the program when button is clicked"""
+        root.quit()  # close the root GUI window
+    close_root = MyButton(nav_frame, "Exit Program", row=0, col=1, command=exit_prog)
+
     # main GUI loop
     root.mainloop()
 
--- myGUI.py
+++ myGUI.py
@@ -78,8 +78,6 @@ class MyFrame(Frame):
     Expected Positional Arguments:
         1. Frame's Parent Window
             master: str = root
-        2. Frame's Label
-            text: str = 'Button Frame'
 
     Expected Keyword Arguments:
         row= Frame's Grid Row
@@ -137,15 +135,133 @@ class MyButton(Button):
         b = MyButton(root, 'Exit', row=5, col=0, rowspan=3, command=close)
     """
     def __init__(self, parent, text, *, row, col, rowspan=1, colspan=1, command=None):
-        Button.__init__(self, master=parent)
+        Button.__init__(self, parent)
         self['text'] = text
-        self.command = command
+        self['command'] = command
         self.grid(row=row,
                   column=col,
                   rowspan=rowspan,
                   columnspan=colspan)
 
 
-'''class MyLabel(Label):
-    """Custom syntax for tkinter Labels"""
-    def __init__(self, parent, text, row, column):'''
+# label class
+class MyLabel(Label):
+    """Custom syntax for creating a tkinter Label.
+
+    Expected Positional Arguments:
+        1. Label's Parent Window / Frame
+            parent: str = root
+        2. Label's Text
+            text: str = 'I'm a Label'
+
+    Expected Keyword Arguments:
+        row= Label's Grid Row
+            row: int = 0
+        col= Label's Grid Column
+            col: int = 0
+
+    Optional Keyword Arguments:
+        rowspan= Label's Grid Rowspan
+            rowspan: int = 1
+        colspan= Label's Grid Columnspan
+            colspan: int = 1
+        var= Label Text's Control Variable (StringVar)
+            var: str = tkvar
+        img= Label's Static Image
+            img: str = '/path/to/img/image.png'
+
+    Examples:
+        l = MyLabel(main_frame, 'label text', row=1, col=0
+        l = MyLabel(main_frame, 'image', row=0, col=0, img='images/img.png')
+    """
+    def __init__(self, parent, text, *, row, col, rowspan=1, colspan=1, var=None, img=None):
+        Label.__init__(self, parent)
+        self['text'] = text
+        self['textvariable'] = var
+        self['image'] = img
+        self.grid(row=row,
+                  column=col,
+                  rowspan=rowspan,
+                  columnspan=colspan)
+
+
+# radiobutton class
+class MyRadio(Radiobutton):
+    """Custom syntax for creating a tkinter Radio Button.
+
+    Expected Positional Arguments:
+        1. Radio Button's Parent Window / Frame
+            parent: str = root
+        2. Radio Button's Text
+            text: str = 'I'm a Radio Button'
+
+    Expected Keyword Arguments:
+        row= Radio Button's Grid Row
+            row: int = 0
+        col= Radio Button's Grid Column
+            col: int = 0
+
+    Optional Keyword Arguments:
+        rowspan= Radio Button's Grid Rowspan
+            rowspan: int = 1
+        colspan= Radio Button's Grid Columnspan
+            colspan: int = 1
+        var= Radio Button's Control Variable (StringVar or IntVar)
+            var: str = tkvar
+        val= Radio Button's Control Variable's Value
+            val: str | int = 'selection' | 1
+
+    Examples:
+        r = MyRadio(main_frame, 'Radio Button', row=2, col=0, var=variable, val=1)
+        r = MyRadio(main_frame, 'Radio Button', row=2, col=0, var=variable, val=2)
+    """
+    def __init__(self, parent, text, *, row, col, rowspan=1, colspan=1, var=None, val=None):
+        Radiobutton.__init__(self, parent)
+        self['text'] = text
+        self['variable'] = var
+        self['value'] = val
+        self.grid(row=row,
+                  column=col,
+                  rowspan=rowspan,
+                  columnspan=colspan)
+
+
+# checkbutton class
+class MyCheck(Checkbutton):
+    """Custom syntax for creating a tkinter Check Button.
+
+    Expected Positional Arguments:
+        1. Check Button's Parent Window / Frame
+            parent: str = root
+        2. Check Button's Text
+            text: str = 'I'm a Radio Button'
+
+    Expected Keyword Arguments:
+        row= Check Button's Grid Row
+            row: int = 0
+        col= Check Button's Grid Column
+            col: int = 0
+
+    Optional Keyword Arguments:
+        rowspan= Check Button's Grid Rowspan
+            rowspan: int = 1
+        colspan= Check Button's Grid Columnspan
+            colspan: int = 1
+        var= Check Button's Control Variable (StringVar or IntVar)
+            var: str = tkvar
+        on= Check Button's Control Variable's 'On' Value
+            on: str | int = 'selection' | 1
+
+    Examples:
+        c = MyCheck(main_frame, 'Check Button', row=3, col=0, var=variable, on='option1')
+        c = MyCheck(main_frame, 'Check Button', row=3, col=0, var=variable, on='option2')
+    """
+    def __init__(self, parent, text, *, row, col, rowspan=1, colspan=1, var=None, on=None):
+        Checkbutton.__init__(self, parent)
+        self['text'] = text
+        self['variable'] = var
+        self['onvalue'] = on
+        self.grid(row=row,
+                  column=col,
+                  rowspan=rowspan,
+                  columnspan=colspan)
